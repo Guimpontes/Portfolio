e150aeb1f8ad10cf3a738ddf00c16c8a16f4276c
Guimpontes <guimotapontes624@gmail.com>
2022-12-29 03:04:05 -0300

    deploy

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,14 @@ import * as C from './style';
 export default function Footer() {
   return (
     <C.Footer>
-      <a href="https://github.com/Guimpontes"><i className="uil uil-github"></i></a>
-      <a href="https://www.linkedin.com/in/guilherme-pontes-a48a99231/"><i className="uil uil-linkedin"></i></a>
+      <C.FooterCredits>
+        <h3>Designed and Developed by Guilherme Pontes</h3>
+      </C.FooterCredits>
+
+      <C.FooterLinks>
+        <a href="https://github.com/Guimpontes"><i className="uil uil-github"></i></a>
+        <a href="https://www.linkedin.com/in/guilherme-pontes-a48a99231/"><i className="uil uil-linkedin"></i></a>
+      </C.FooterLinks>
     </C.Footer>
   )
 }
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -4,9 +4,13 @@ export const Footer = styled.div`
     width: 100%;
     height: 50px;
     background-color: #733702;
-    text-align: end;
     padding: 10px 15px;
-    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const FooterLinks = styled.div`
 
     a {
         font-size: 1.5em;
@@ -16,5 +20,12 @@ export const Footer = styled.div`
     
     i:last-child {
         margin-right: 0;
+    }
+`
+
+export const FooterCredits = styled.div`
+    h3 {
+        font-size: 1rem;
+        color: #F2F2F2;
     }
 `
