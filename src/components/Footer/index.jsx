c1373f4729f3dc49adcd9d334d1a15a9fa718416
Guimpontes <guimotapontes624@gmail.com>
2022-12-29 03:22:51 -0300

    deploy

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,6 +8,10 @@ export default function Footer() {
         <h3>Designed and Developed by Guilherme Pontes</h3>
       </C.FooterCredits>
 
+      <C.FoooterCopyRight>
+        <h3>Copyright <i className="uil uil-copyright"></i> 2022 GMP</h3>
+      </C.FoooterCopyRight>
+
       <C.FooterLinks>
         <a href="https://github.com/Guimpontes"><i className="uil uil-github"></i></a>
         <a href="https://www.linkedin.com/in/guilherme-pontes-a48a99231/"><i className="uil uil-linkedin"></i></a>
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -4,13 +4,35 @@ export const Footer = styled.div`
     width: 100%;
     height: 50px;
     background-color: #733702;
-    padding: 10px 15px;
+    padding: 0 10px;
     display: flex;
     justify-content: space-between;
     align-items: center;
 `
+export const FooterCredits = styled.div`
+    width: 33%;
+
+    h3 {
+        font-size: 1rem;
+        font-weight: 600;
+        color: #F2F2F2;
+    }
+`
+
+export const FoooterCopyRight = styled.div`
+    width: 33%; 
+    text-align: center;
+
+    h3 {
+        font-size: 1rem;
+        font-weight: 600;
+        color: #F2F2F2;
+    }
+`
 
 export const FooterLinks = styled.div`
+    width: 33%;
+    text-align: end;
 
     a {
         font-size: 1.5em;
@@ -20,12 +42,5 @@ export const FooterLinks = styled.div`
     
     i:last-child {
         margin-right: 0;
-    }
-`
-
-export const FooterCredits = styled.div`
-    h3 {
-        font-size: 1rem;
-        color: #F2F2F2;
     }
 `
