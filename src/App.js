9f2694efd905096397a5017fccb20224ac6f204c
Guimpontes <guimotapontes624@gmail.com>
2022-12-29 02:33:36 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyles';
 import { ThemeProvider } from "styled-components"
 import light from './styles/Theme/light';
